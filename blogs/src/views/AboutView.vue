103f41f82c3345fa3b22d90baba7cc24143fd39f
wuheze <631579243@qq.com>
2023-09-27 09:33:54 +0800

    blogs init
    Signed-off-by: wuheze <631579243@qq.com>

---
+++ blogs/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <n-button>naive-ui</n-button>
+  </div>
+</template>
+
+<script setup>
+import { NButton } from 'naive-ui'
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
